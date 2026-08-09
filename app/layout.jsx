39c20de49e1b0a885f9789e1b16425f64647abdf
Gabby-design <gabrieltolulope50@gmail.com>
2026-08-09 04:17:20 -0700

    Fix layout hydration warning caused by Grammarly

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Syne:wght@400..800&display=swap" rel="stylesheet" />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <ToastProvider />
         <div id="root">
           {children}
